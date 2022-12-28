66aff9b1a36e0f5f64cc67fd0a8e86ec84857552
Yeon Sug Lee <tommy_lee_62@hotmail.com>
2022-12-28 13:36:22 -0500

    Commit from Theia.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,14 +10,14 @@ public_users.post("/register", (req,res) => {
     const password = req.body.password;
     
     if (username && password) {
-        if (!doesExist(username)) { 
+        if (!isValid(username)) { 
         users.push({"username":username,"password":password});
         return res.status(200).json({message: "User successfully registred. Now you can login"});
         } else {
         return res.status(404).json({message: "User already exists!"});    
         }
     } 
-    return res.status(404).json({message: "Unable to register user."});      
+    return res.status(404).json({message: "Unable to register user. Please add credentials."});      
 });
 
 // Get the book list available in the shop
